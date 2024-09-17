8129c808bf4ad65336c121419d408740b14b0bd8
Clare Zureich <zureichc@tcd.ie>
2024-09-17 18:26:42 +0100

    Problem #1 completed

---
+++ problemSets/PS01/PS1_answers_CZ.R
@@ -0,0 +1,54 @@
+#####################
+# load libraries
+# set wd
+# clear global .envir
+#####################
+
+# remove objects
+rm(list=ls())
+# detach all libraries
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search()))==1, TRUE, FALSE)]
+  package.list <- setdiff(package.list, basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package,  character.only=TRUE)
+}
+detachAllPackages()
+
+# load libraries
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,  dependencies = TRUE)
+  sapply(pkg,  require,  character.only = TRUE)
+}
+
+# here is where you load any necessary packages
+# ex: stringr
+# lapply(c("stringr"),  pkgTest)
+
+lapply(c(),  pkgTest)
+
+#####################
+# Problem 1
+#####################
+
+scores <- c(105, 69, 86, 100, 82, 111, 104, 110, 87, 108, 87, 90, 94, 113, 112, 98, 80, 97, 95, 111, 114, 89, 95, 126, 98)
+
+#Find the mean, standard deviation, and standard error of the IQ scores
+mean <-mean(scores)
+sd <- sd(scores)
+se <- sd/sqrt(length(scores))
+
+# Find the confidence interval, using z-score of 1.645, for 90%
+
+lower_90 <- mean - 1.645*se
+upper_90 <- mean + 1.645*se
+lower_90
+upper_90
+
+#####################
+# Problem 2
+#####################
+
+expenditure <- read.table("https://raw.githubusercontent.com/ASDS-TCD/StatsI_Fall2024/main/datasets/expenditure.txt", header=T)
